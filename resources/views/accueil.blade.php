8428c44c1c726896e40844367c2c37aa9fbd7b21
dinno1986 <dimitri.castell@hotmail.fr>
2017-09-12 15:30:46 +0200

    formulaire recupere les données

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page d'accueil</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Petites annonces</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Déposer une annonce</a></li>
+        <li><a href="#">Offres</a></li>
+        <li><a href="#">Mes annonces</a></li>
+        <li><a href="/inscription">Inscription</a></li>
+        <li><a href="/admin">Connexion</a></li> 
+      </ul>
+    </div>
+ 
+  </div>
+</nav>
+
+
+
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ resources/views/inscription.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscription</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+
+
+
+<div class="row">
+	<div class="col-md-4">
+	</div>
+
+	<div class="col-md-4">
+		<form method='post' id="form" enctype="multipart/form-data" action='http://127.0.0.1:8000/admin/users/add-save''>
+
+		{{csrf_field()}}
+		
+			<legend>
+				<center>Formulaire d'inscription</center>
+			</legend>
+			
+			  <div class="form-group">
+			    <label for="name">name</label>
+			    <input type="text" name="name" class="form-control" id="name" placeholder="name">
+			  </div>
+
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Email address</label>
+			    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="mail">
+			  </div>
+
+			  <div class="form-group">
+			    <label for="exampleInputFile">File input</label>
+			    <input type="file" name="photo" id="exampleInputFile">
+			  </div>
+
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password</label>
+			    <input type="password" name="password" class="form-control"  id="exampleInputPassword1" placeholder="Password">
+			  </div>
+
+	  		 
+	  		 	<button type="submit" class="btn btn-success">Submit</button>
+	  		 
+	  		
+  		</form>
+  	</div>
+
+  	<div class="col-md-4">
+  	</div>
+
+</div>
+
+
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
